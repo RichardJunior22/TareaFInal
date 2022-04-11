3013c13723fc3ddb8515d9219b630632b00d4777
RichardJunior22 <89370374+RichardJunior22@users.noreply.github.com>
2022-04-11 00:01:19 -0400

    Add files via upload

---
+++ src/main/java/componentes/ObtenerDatos.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package componentes;
+
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Richard Fortunato
+ */
+public class ObtenerDatos {
+
+    public JTextField getTextFieldApellido() {
+        return textFieldApellido;
+    }
+
+    public void setTextFieldApellido(JTextField textFieldApellido) {
+        this.textFieldApellido = textFieldApellido;
+    }
+
+    public JTextField getTextFieldCorreo() {
+        return textFieldCorreo;
+    }
+
+    public void setTextFieldCorreo(JTextField textFieldCorreo) {
+        this.textFieldCorreo = textFieldCorreo;
+    }
+
+    public JTextField getTextFieldN() {
+        return textFieldN;
+    }
+
+    public void setTextFieldN(JTextField textFieldN) {
+        this.textFieldN = textFieldN;
+    }
+
+    public JTextField getTextFieldNumero() {
+        return textFieldNumero;
+    }
+
+    public void setTextFieldNumero(JTextField textFieldNumero) {
+        this.textFieldNumero = textFieldNumero;
+    }
+
+    public JPasswordField getTextFieldPass() {
+        return textFieldPass;
+    }
+
+    public void setTextFieldPass(JPasswordField textFieldPass) {
+        this.textFieldPass = textFieldPass;
+    }
+
+    public JPasswordField getTextFieldPassC() {
+        return textFieldPassC;
+    }
+
+    public void setTextFieldPassC(JPasswordField textFieldPassC) {
+        this.textFieldPassC = textFieldPassC;
+    }
+
+    public JTextField getTextFielnUsuario() {
+        return textFielnUsuario;
+    }
+
+    public void setTextFielnUsuario(JTextField textFielnUsuario) {
+        this.textFielnUsuario = textFielnUsuario;
+    }
+    private javax.swing.JTextField textFieldApellido;
+    private javax.swing.JTextField textFieldCorreo;
+    private javax.swing.JTextField textFieldN;
+    private javax.swing.JTextField textFieldNumero;
+    private javax.swing.JPasswordField textFieldPass;
+    private javax.swing.JPasswordField textFieldPassC;
+    private javax.swing.JTextField textFielnUsuario;
+}
